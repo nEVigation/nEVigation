5c76957b68d74253cacdaf952f71e5a6c4380362
TheSoundofGoodbye <karbehemoth@gmail.com>
2021-11-11 09:00:14 +0900

    update

---
+++ nEVigation/src/main/java/Member/controller/MemberController.java
@@ -0,0 +1,20 @@
+package Member.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import Member.service.face.MemberService;
+
+@Controller
+@RequestMapping(value="/member")
+public class MemberController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
+	
+	@Autowired private MemberService memberService;
+	
+	
+}
---
+++ nEVigation/src/main/java/Member/service/face/MemberService.java
@@ -0,0 +1,5 @@
+package Member.service.face;
+
+public interface MemberService {
+
+}
